ce9c45991337bfdec44611a59f08f09856b55b1b
Korkeat W <korkeat.w@gmail.com>
2019-02-18 17:09:24 +0700

    Change JSON properties of AuthenticationResponse to camel-case

--- src/main/java/com/korkeat/mobileregistration/entity/AuthenticationResponse.java
+++ src/main/java/com/korkeat/mobileregistration/entity/AuthenticationResponse.java
@@ -1,5 +1,6 @@
 package com.korkeat.mobileregistration.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,6 +13,10 @@ import java.util.Date;
 public class AuthenticationResponse {
     private String username;
     private String token;
+
+    @JsonProperty("issued_at")
     private Date issuedAt;
+
+    @JsonProperty("expired_at")
     private Date expiredAt;
 }
